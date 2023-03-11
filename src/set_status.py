bcd088c140e377bf52ed2e90128c77f7f84aa014
Ryo Shirai <65144528+ryo-shirai421@users.noreply.github.com>
2023-03-12 01:46:46 +0900

    add docstring to chage_status function

--- src/set_status.py
+++ src/set_status.py
@@ -2,12 +2,21 @@ import datetime
 
 
 def change_status(current_time, past_time, past_status, sensor_values):
-    '''
-    time: 現在時刻
-    past_time: 前のステータスに変化した時の時刻
-    past_status: 前のステータス
-    sensor_value: センサの値 (1*8)の配列
-    '''
+    """
+    ステータスの変化に関する関数
+    
+    Parameters
+    ----------
+    current_time    : 現在時刻
+    past_time       : 前のステータスに変化した時の時刻
+    past_status     : 前のステータス
+    sensor_value    : センサの値 (1*8)の配列
+
+    Returns
+    -------
+    current_status  : 現在時刻のステータス
+    change_time     : ステータスが変化した時間
+    """
 
     if any(value >= 30 for value in sensor_values):
         current_status = 1
@@ -23,7 +32,7 @@ def change_status(current_time, past_time, past_status, sensor_values):
             else:
                 current_status = 2
 
-    # ここから先は編集しない
+    ### ------------------------------ ここから先は編集しない -----------------------------------###
     change_time = current_time if current_status != past_status else past_time
 
     return current_status, change_time

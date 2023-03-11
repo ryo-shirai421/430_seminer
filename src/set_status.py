370932672e1ff42ec92832c94624f42c1d6c74bc
Ryo Shirai <65144528+ryo-shirai421@users.noreply.github.com>
2023-03-12 01:29:44 +0900

    edit set_status.py & sandbox.py

--- src/set_status.py
+++ src/set_status.py
@@ -1,32 +1,32 @@
 import datetime
 
 
-def change_status(now_time, past_time, past_status, values):
+def change_status(current_time, past_time, past_status, sensor_values):
     '''
     time: 現在時刻
     past_time: 前のステータスに変化した時の時刻
     past_status: 前のステータス
-    value: センサの値 (1*8)の配列
+    sensor_value: センサの値 (1*8)の配列
     '''
 
-    if any(v >= 30 for v in values):
-        new_status = 1
+    if any(value >= 30 for value in sensor_values):
+        current_status = 1
 
     else:
         if (past_status == 0):
-            new_status = 0
+            current_status = 0
         elif (past_status == 1):
-            new_status = 2
+            current_status = 2
         else:
-            if (now_time - datetime.timedelta(minutes=1) >= past_time):
-                new_status = 0
+            if (current_time - datetime.timedelta(minutes = 1) >= past_time):
+                current_status = 0
             else:
-                new_status = 2
+                current_status = 2
 
-    #ここから先は編集しない
-    if (new_status != past_status):
-        change_time = now_time
+    # ここから先は編集しない
+    if (current_status != past_status):
+        change_time = current_time
     else:
         change_time = past_time
 
-    return new_status, change_time
+    return current_status, change_time

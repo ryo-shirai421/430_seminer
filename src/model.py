34477a0ecd9cab5455651e67adbfef17b7a6be23
Y D <yd@d47.private>
2023-03-13 19:06:07 +0900

    knn

---
+++ src/model.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+import pickle
+
+
+def get_status(current_time, past_time, past_status, sensor_values):
+    knn = pickle.load(open('regmodel.pkl', 'rb'))
+    """
+    センサーの値と過去の情報から，現在の状態を決める関数
+
+    引数
+    ----------
+    current_time       : 現在時刻
+    past_time          : 前のステータスに変化した時の時刻
+    past_status        : 前のステータス
+    sensor_value       : センサの値 (1x8) の配列
+    返り値
+    -------
+    current_status     : 現在時刻のステータス
+    state_changed_time : ステータスが変化した時間
+    """
+
+    # ------------------------------- ここから -----------------------------------
+    current_status = knn.predict(sensor_values)
+    if current_status == 0:
+
+        if past_status == 0:
+            current_status = 0
+        elif past_status == 1:
+            current_status = 2
+        else:
+            if current_time - datetime.timedelta(minutes=1) >= past_time:
+                current_status = 0
+            else:
+                current_status = 2
+
+    # ------------------------------- ここまで -----------------------------------
+
+    # 状態が変わったら，その時刻を保存しておく．
+    state_changed_time = current_time if current_status != past_status else past_time
+
+    return current_status, state_changed_time
+
+def train_model():
+    x_train = pd.read_csv('../data/train_data.csv')
+    y_train = pd.read_csv('../data/train_data.csv')
+    knn = KNeighborsClassifier(n_neighbors=2)
+    knn.fit(x_train, y_train)
+    pickle.dump(knn, open('../params/knn_model.pkl', 'wb'))
+
+
+def main():
+    train_model()
+if __name__ == "__main__":
+    main()

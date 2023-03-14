28f1bf2de3b28fda247da6fce5b48cc4c70d2869
Ryo Shirai <65144528+ryo-shirai421@users.noreply.github.com>
2023-03-14 15:37:33 +0900

    Update get_status.py

--- src/get_status.py
+++ src/get_status.py
@@ -16,9 +16,9 @@ def get_status(current_time, past_time, past_status, sensor_values):
     current_status     : 現在時刻のステータス
     state_changed_time : ステータスが変化した時間
     """
-    # Step1: 現状のステータスが変わらない事を確認しよう．
+    # Step1: 現在のステータスが変わらない事を確認しよう．
     # Step2: 温度が上がると在席，下がると離席となるようにプログラムを実装してみよう．
-    # Step3: 過去の状態を利用して一時離席を判断できるようにしよう．
+    # Step3: 過去の状態を利用して，一時離席を判断できるようにしよう．
     # Advance1: 在席と離席の判断を機械学習モデルにやらせてみよう．
     # Advance2: 決定木モデルで予測してみよう．
     # Advance3: 決定木がどのように判断しているかを見てみよう．(sandbox.ipynbで行う)

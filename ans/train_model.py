dab680d6e61a3cbc87ea4c274b28fd534c2cbfbb
ryo-shirai <ryo.shirai@linecorp.com>
2023-03-15 15:11:41 +0900

    edit model code

--- ans/train_model.py
+++ ans/train_model.py
@@ -7,20 +7,21 @@ from sklearn import tree
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def train_model():
+MODELS = {"tree": tree.DecisionTreeClassifier(), "knn": KNeighborsClassifier(n_neighbors=2)}
+
+
+def train_model(model_name="knn"):
     parent_dir = Path(__file__).resolve().parents[1]
     # 訓練データの読み込み
     X = pd.read_csv(parent_dir.joinpath("data", "train_data.csv"), header=None).values
     # 正解ラベルの読み込み
     y = np.ravel(pd.read_csv(parent_dir.joinpath("data", "train_label.csv"), header=None).values)
     # モデルの読み込み
-    model = KNeighborsClassifier(n_neighbors=2)
-    # model = tree.DecisionTreeClassifier()
+    model = MODELS[model_name]
     # モデルの学習
     model.fit(X, y)
     # 学習済みのモデルを保存
-    pickle.dump(model, open(parent_dir.joinpath("params", "knn_model.pkl"), "wb"))
-    # pickle.dump(model, open(parent.joinpath("params", "tree_model.pkl"), "wb"))
+    pickle.dump(model, open(parent_dir.joinpath("params", "{}_model.pkl".format(model_name)), "wb"))
 
 
 if __name__ == "__main__":
